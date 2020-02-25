a3e66e1f23c1d9481fb8b2477c166b6d0fdf8d61
xie258789 <544482194@qq.com>
2020-02-25 11:43:43 +0800

    【feat】图片压缩

---
+++ demo-image/src/main/java/com/example/demo/image/compress/CompressDemo.java
@@ -0,0 +1,129 @@
+package com.example.demo.image.compress;
+
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import javax.imageio.ImageIO;
+/**
+ * 图像压缩
+ */
+public class CompressDemo {
+    public static void main(String[] args) throws IOException {
+        String inputFile = "demo-image/temp/test.jpeg";
+        String outputFile = "demo-image/temp/output.jpeg";
+
+//        zipWidthHeightImageFile(new File(inputFile), new File(outputFile),425,638,0.7f);
+
+        zipImageFile(new File(inputFile), new File(outputFile),500,0,0.7f);
+
+        //zipImageFile(new File("C:\\spider\\3.jpg"),new File("C:\\spider\\3-3.jpg"),425,638,0.7f);
+
+        System.out.println("ok");
+    }
+
+    /**
+     * 根据设置的宽高等比例压缩图片文件<br> 先保存原文件，再压缩、上传
+     * @param oldFile  要进行压缩的文件
+     * @param newFile  新文件
+     * @param width  宽度 //设置宽度时（高度传入0，等比例缩放）
+     * @param height 高度 //设置高度时（宽度传入0，等比例缩放）
+     * @param quality 质量
+     * @return 返回压缩后的文件的全路径
+     */
+    public static String zipImageFile(File oldFile,File newFile, int width, int height,float quality) {
+        if (oldFile == null) {
+            return null;
+        }
+        try {
+            /** 对服务器上的临时文件进行处理 */
+            Image srcFile = ImageIO.read(oldFile);
+            int w = srcFile.getWidth(null);
+            int h = srcFile.getHeight(null);
+            double bili;
+            if(width>0){
+                bili=width/(double)w;
+                height = (int) (h*bili);
+            }else{
+                if(height>0){
+                    bili=height/(double)h;
+                    width = (int) (w*bili);
+                }
+            }
+
+            String srcImgPath = newFile.getAbsoluteFile().toString();
+            System.out.println(srcImgPath);
+            String subfix = "jpg";
+            subfix = srcImgPath.substring(srcImgPath.lastIndexOf(".")+1,srcImgPath.length());
+
+            BufferedImage buffImg = null;
+            if(subfix.equals("png")){
+                buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            }else{
+                buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            }
+
+            Graphics2D graphics = buffImg.createGraphics();
+            graphics.setBackground(new Color(255,255,255));
+            graphics.setColor(new Color(255,255,255));
+            graphics.fillRect(0, 0, width, height);
+            graphics.drawImage(srcFile.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0, 0, null);
+
+            ImageIO.write(buffImg, subfix, new File(srcImgPath));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newFile.getAbsolutePath();
+    }
+
+    /**
+     * 按设置的宽度高度压缩图片文件<br> 先保存原文件，再压缩、上传
+     * @param oldFile  要进行压缩的文件全路径
+     * @param newFile  新文件
+     * @param width  宽度
+     * @param height 高度
+     * @param quality 质量
+     * @return 返回压缩后的文件的全路径
+     */
+    public static String zipWidthHeightImageFile(File oldFile,File newFile, int width, int height,float quality) {
+        if (oldFile == null) {
+            return null;
+        }
+        String newImage = null;
+        try {
+            /** 对服务器上的临时文件进行处理 */
+            Image srcFile = ImageIO.read(oldFile);
+
+            String srcImgPath = newFile.getAbsoluteFile().toString();
+            System.out.println(srcImgPath);
+            String subfix = "jpg";
+            subfix = srcImgPath.substring(srcImgPath.lastIndexOf(".")+1,srcImgPath.length());
+
+            BufferedImage buffImg = null;
+            if(subfix.equals("png")){
+                buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            }else{
+                buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            }
+
+            Graphics2D graphics = buffImg.createGraphics();
+            graphics.setBackground(new Color(255,255,255));
+            graphics.setColor(new Color(255,255,255));
+            graphics.fillRect(0, 0, width, height);
+            graphics.drawImage(srcFile.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0, 0, null);
+
+            ImageIO.write(buffImg, subfix, new File(srcImgPath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newImage;
+    }
+}
