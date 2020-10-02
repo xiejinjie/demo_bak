4f1b940e5a9dd69c82880653086b5dc722c57800
xie258789 <544482194@qq.com>
2020-10-02 17:37:23 +0800

    【feat】graphql demo

---
+++ demo-graphql/src/main/java/com/demo/graphql/Application.java
@@ -0,0 +1,15 @@
+package com.demo.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jjxiek
+ * @since 2019/11/18 15:39
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ demo-graphql/src/main/java/com/demo/graphql/TestController.java
@@ -0,0 +1,17 @@
+package com.demo.graphql;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author jj
+ * @since 2020/9/23 23:13
+ */
+@Controller
+public class TestController {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hello";
+    }
+}
