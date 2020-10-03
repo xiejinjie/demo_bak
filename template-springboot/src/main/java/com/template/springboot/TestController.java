5c4009c8a9f959f9c7ac42eca1b4797ac04825de
xie258789 <544482194@qq.com>
2020-10-03 10:37:06 +0800

    【fix】rest请求500

--- template-springboot/src/main/java/com/template/springboot/TestController.java
+++ template-springboot/src/main/java/com/template/springboot/TestController.java
@@ -2,6 +2,7 @@ package com.template.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author jj
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello";
     }
